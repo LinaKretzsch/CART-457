56db404efac1fb300b953d434df9bf9aa146466a
Lina Kretzschmar <lina.kretzschmar@hotmail.de>
2021-03-02 20:34:17 -0500

    Revert "Revert "Revert "Revert "Revert "Revert "Working on SPA implementation""""""
    This reverts commit 9443c0889d441248143b1d38292665952f99bf08.

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+// Created this code by following the tutorial https://www.youtube.com/watch?v=6BozpmSjk-Y
+
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static"))); //whenever the path has /static inside of it, we will server the static directory
+
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve("frontend", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html")); //will default to this file
 });
 
-app.listen(process.env.PORT || 5060, () => console.log("Server running..."));
+app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
