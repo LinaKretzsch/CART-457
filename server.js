77c875b14baedd0be34e539efb42431ccfac6052
Lina Kretzschmar <lina.kretzschmar@hotmail.de>
2021-04-28 16:54:21 -0400

    Removed files and trying out things for heroku
    Removed files and trying out things for heroku

--- server.js
+++ server.js
@@ -180,6 +180,6 @@ async function secondFunction() {
 
 
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
